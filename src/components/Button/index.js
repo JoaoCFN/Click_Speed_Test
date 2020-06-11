9b9001eba70ab1cdbe52f24cb72581bbb3fd7c3b
João Neto <joaoneto.20fsa@gmail.com>
2020-06-11 17:11:12 -0300

    Initial commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+
+const Button = () => {
+    return (
+        <button>Clique para começar</button>
+    );
+}
+
+export default Button;
---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./index.css";
+
+const Title = () => {
+    return (
+        <h1 className="title">Click Speed Test</h1>
+    );
+}
+
+export default Title;
